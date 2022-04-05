039321710f9b6b946d70b33a0a24056225e53bfb
Milo <milo.christiansen@gmail.com>
2022-04-05 16:45:16 -0400

    Added some godoc to the parser errors.

--- parse/errors.go
+++ parse/errors.go
@@ -24,30 +24,35 @@ package parse
 
 import "fmt"
 
+// ErrBadDate is returned by the parser when it attempts to consume a invalid date.
 type ErrBadDate int
 
 func (err ErrBadDate) Error() string {
 	return fmt.Sprintf("Malformed transaction date on line: %v", int(err))
 }
 
+// ErrBadAmount is returned by the parser when it attempts to consume an amount that is out of the valid range.
 type ErrBadAmount int
 
 func (err ErrBadAmount) Error() string {
 	return fmt.Sprintf("Amount value out of range on line: %v", int(err))
 }
 
+// ErrUnexpectedEnd is returned by the parser when the end of input is found unexpectedly.
 type ErrUnexpectedEnd int
 
 func (err ErrUnexpectedEnd) Error() string {
 	return fmt.Sprintf("Unexpected end of input on line: %v", int(err))
 }
 
+// ErrMalformed is returned by the parser when it finds a malformed transaction.
 type ErrMalformed int
 
 func (err ErrMalformed) Error() string {
 	return fmt.Sprintf("Malformed transaction on line: %v", int(err))
 }
 
+// ErrMalformedTagLine is returned by the parser when it attempts to consume a tag line that is malformed.
 type ErrMalformedTagLine int
 
 func (err ErrMalformedTagLine) Error() string {
